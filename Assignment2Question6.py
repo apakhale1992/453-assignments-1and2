c39e5b6aef11143f124ef2700ba1fe2db7640b6b
apakhale1992 <a.pakhale1992@gmail.com>
2026-06-19 02:17:53 +0530

    Add files via upload

---
+++ Assignment2Question6.py
@@ -0,0 +1,10 @@
+print("Enter first number : ")
+no1 = int(input())
+
+print("Enter second number : ")
+no2 = int(input())
+
+print("Addition is ", no1 + no2)
+print("Subtraction is ", no1 - no2)
+print("Division is ", no1 / no2)
+print("Multiplication is ", no1 * no2)
---
+++ Assignment2Question9.py
@@ -0,0 +1,7 @@
+print("Please enter your name")
+name = input()
+
+print("Please enter your age")
+age = int(input())
+
+print(f"Hello {name}, you will turn {age+1} next year")
